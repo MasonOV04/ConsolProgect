ddf7518f820d5238dcd58dd9d5e7901b9d2f4ddd
Oleg <masonov4@mail.ru>
2023-10-08 09:15:49 +0300

    что-то

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите первое число;");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число;");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a*a == b) { 
+    Console.WriteLine("Является)");}
+
+else
+{
+     Console.WriteLine("Не является");
+}
